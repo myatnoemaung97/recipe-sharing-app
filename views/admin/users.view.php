7befc5b04287fbdd150fc836601fadf85fd98ec1
myatnoemaung97 <myatnoemaung001@gmail.com>
2023-09-20 10:58:05 +0630

    adminLTE tables

--- views/admin/users.view.php
+++ views/admin/users.view.php
@@ -8,16 +8,88 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin</title>
+  <title>Admin - Users</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="/resources/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/resources/dist/css/adminlte.min.css">
-  <script src="https://kit.fontawesome.com/807f2d6ec6.js" crossorigin="anonymous"></script>
+  <!-- <link rel="stylesheet" href="/resources/dist/css/adminlte.min.css"> -->
+  <link rel="stylesheet" href="/resources/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="/resources/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="/resources/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+
+  <link rel="stylesheet" href="/resources/dist/css/adminlte.min.css?v=3.2.0">
+
   <script src="/resources/js/general.js"></script>
+
+  <script nonce="4364594b-9c68-4a27-89f4-773ce80bebf7">
+    (function(w, d) {
+      ! function(a, b, c, d) {
+        a[c] = a[c] || {};
+        a[c].executed = [];
+        a.zaraz = {
+          deferred: [],
+          listeners: []
+        };
+        a.zaraz.q = [];
+        a.zaraz._f = function(e) {
+          return async function() {
+            var f = Array.prototype.slice.call(arguments);
+            a.zaraz.q.push({
+              m: e,
+              a: f
+            })
+          }
+        };
+        for (const g of ["track", "set", "debug"]) a.zaraz[g] = a.zaraz._f(g);
+        a.zaraz.init = () => {
+          var h = b.getElementsByTagName(d)[0],
+            i = b.createElement(d),
+            j = b.getElementsByTagName("title")[0];
+          j && (a[c].t = b.getElementsByTagName("title")[0].text);
+          a[c].x = Math.random();
+          a[c].w = a.screen.width;
+          a[c].h = a.screen.height;
+          a[c].j = a.innerHeight;
+          a[c].e = a.innerWidth;
+          a[c].l = a.location.href;
+          a[c].r = b.referrer;
+          a[c].k = a.screen.colorDepth;
+          a[c].n = b.characterSet;
+          a[c].o = (new Date).getTimezoneOffset();
+          if (a.dataLayer)
+            for (const n of Object.entries(Object.entries(dataLayer).reduce(((o, p) => ({
+                ...o[1],
+                ...p[1]
+              })), {}))) zaraz.set(n[0], n[1], {
+              scope: "page"
+            });
+          a[c].q = [];
+          for (; a.zaraz.q.length;) {
+            const q = a.zaraz.q.shift();
+            a[c].q.push(q)
+          }
+          i.defer = !0;
+          for (const r of [localStorage, sessionStorage]) Object.keys(r || {}).filter((t => t.startsWith("_zaraz_"))).forEach((s => {
+            try {
+              a[c]["z_" + s.slice(7)] = JSON.parse(r.getItem(s))
+            } catch {
+              a[c]["z_" + s.slice(7)] = r.getItem(s)
+            }
+          }));
+          i.referrerPolicy = "origin";
+          i.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(a[c])));
+          h.parentNode.insertBefore(i, h)
+        };
+        ["complete", "interactive"].includes(b.readyState) ? zaraz.init() : a.addEventListener("DOMContentLoaded", zaraz.init)
+      }(w, d, "zarazData", "script");
+    })(window, document);
+  </script>
+</head>
+
+<script src="https://kit.fontawesome.com/807f2d6ec6.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -43,81 +115,83 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Main content -->
       <div class="content">
         <div class="container-fluid">
-          <div class='d-flex justify-content-between'>
-            <h5>Total - <?= count($users) ?></h5>
-            <form action="/home/admin/users" method="GET">
-              <label for="">Sort by: </label>
-              <select name="sort" id="">
-                <option value="id">Id</option>
-                <option value="name">Name</option>
-                <option value="email">Email</option>
-                <option value="created">Created</option>
-                <option value="updated">Updated</option>
-              </select>
-              <button type="submit" class="btn btn-primary btn-sm">Sort</button>
-            </form>
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-body">
+                  <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Created</th>
+                        <th>Updated</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php foreach ($users as $user) : ?>
+                        <tr>
+                          <td><?= $user['id'] ?></td>
+                          <td><?= $user['name'] ?></td>
+                          <td><?= $user['email'] ?></td>
+                          <td><?= $user['created'] ?></td>
+                          <td><?= $user['updated'] ?></td>
+                          <td><button class="btn btn-danger" onclick="confirmDeleteProfile(<?= $user['id'] ?>, true)">Delete</button></td>
+                        </tr>
+                      <?php endforeach; ?>
+                    </tbody>
+                  </table>
+                </div>
+
+              </div>
+            </div>
+            <!-- /.content -->
           </div>
+          <!-- /.content-wrapper -->
 
-          <table class="table table-secondary table-striped table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Created</th>
-                <th scope="col">Updated</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach ($users as $user) : ?>
-                <tr>
-                  <td><?= $user['id'] ?></td>
-                  <td><?= $user['name'] ?></td>
-                  <td><?= $user['email'] ?></td>
-                  <td><?= $user['created'] ?></td>
-                  <td><?= $user['updated'] ?></td>
-                  <td><button class="btn btn-danger" onclick="confirmDeleteProfile(<?= $user['id'] ?>, true)">Delete</button></td>
-                </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
+        </div>
+        <!-- ./wrapper -->
 
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-      <!-- Control sidebar content goes here -->
-      <div class="p-3">
-        <h5>Title</h5>
-        <p>Sidebar content</p>
-      </div>
-    </aside>
-    <!-- /.control-sidebar -->
+        <!-- REQUIRED SCRIPTS -->
 
-    <!-- Main Footer -->
-    <footer class="main-footer">
-      <!-- To the right -->
-      <div class="float-right d-none d-sm-inline">
-        Anything you want
-      </div>
-      <!-- Default to the left -->
-      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-    </footer>
-  </div>
-  <!-- ./wrapper -->
+        <!-- jQuery -->
+        <script src="/resources/plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="/resources/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="/resources/dist/js/adminlte.min.js"></script>
+        <script src="/resources/plugins/datatables/jquery.dataTables.min.js"></script>
+        <script src="/resources/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+        <script src="/resources/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="/resources/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+        <script src="/resources/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+        <script src="/resources/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+        <script src="/resources/plugins/jszip/jszip.min.js"></script>
+        <script src="/resources/plugins/pdfmake/pdfmake.min.js"></script>
+        <script src="/resources/plugins/pdfmake/vfs_fonts.js"></script>
+        <script src="/resources/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+        <script src="/resources/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+        <script src="/resources/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
-  <!-- REQUIRED SCRIPTS -->
+        <script src="/resources/dist/js/adminlte.min.js?v=3.2.0"></script>
 
-  <!-- jQuery -->
-  <script src="/resources/plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="/resources/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="/resources/dist/js/adminlte.min.js"></script>
+        <script src="/resources/dist/js/demo.js"></script>
+
+        <script>
+          $(function() {
+            $('#example2').DataTable({
+              "paging": true,
+              "lengthChange": false,
+              "searching": true,
+              "ordering": true,
+              "info": true,
+              "autoWidth": false,
+              "responsive": true,
+            });
+          });
+        </script>
 </body>
 
 </html>
